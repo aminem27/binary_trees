bd2272f9263319a6cd7b57842afc44087e3d6d3f
aminem27 <amine_abbes@hotmail.com>
2023-11-02 12:44:06 -0700

    test_directory

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -6,6 +6,7 @@
  * @value: The value to store in the node.
  * Return: A pointer to to created node or NULL on failure.
  */
+
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = NULL;
